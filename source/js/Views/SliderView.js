264fd243192007ce804afc0244d5235519554ef5
mulder21c <publisher@publisher.name>
2018-08-01 20:54:36 +0900

    Slide Component 접근성 기능 추가
    - tabpanel role 누락 수정
    - 키보드 내비게이션 기능 추가
    - 스크린리더 live 안내 텍스트 구체화 (1/4 => 1/4 slide)

--- source/js/Views/SliderView.js
+++ source/js/Views/SliderView.js
@@ -5,12 +5,13 @@ define(() => {
     el: null,
     wrapperSelector: '.slider-inner',
     itemSelector: '.slider-item',
+    controllerSelector: '.slide-controller',
     btnPrevSelector: '.btn-prev',
     btnNextSelector: '.btn-next',
     indicatorSelector: '.slider-indicator',
-    animateDuration: 700,
     activeClass: 'active',
     startIdx: 0,
+    useController: true,
     useIndicator: false,
     useSRNotation: false
   };
@@ -23,43 +24,48 @@ define(() => {
   SliderView.prototype = {
     init (settings) {
       let me = this;
-      settings = _.extend(defaults, settings);
-      if(!settings.el) throw new Error(tag, 'el is not defined or wrong!');
-      me.el = document.querySelector(settings.el);
+      me.settings = _.extend(defaults, settings);
+
+      if(!me.settings.el) throw new Error(tag, 'el is not defined or wrong!');
+      me.el = document.querySelector(me.settings.el);
       if(!me.el) throw new Error(tag, 'the element is absent!', 'selector : ' + me.el);
 
-      me.sliderWrapper = me.el.querySelector(settings.wrapperSelector);
-      me.slideItems = me.el.querySelectorAll(settings.itemSelector);
-      me.indicator = me.el.querySelector(settings.indicatorSelector);
+      me.sliderWrapper = me.el.querySelector(me.settings.wrapperSelector);
+      me.slideItems = me.el.querySelectorAll(me.settings.itemSelector);
+      me.indicators = me.el.querySelector(me.settings.indicatorSelector);
+      me.controller = me.el.querySelector(me.settings.controllerSelector);
       me.srNotation = me.el.querySelector('.sr-notation');
-      me.btnNext = me.el.querySelector(settings.btnNextSelector);
-      me.btnPrev = me.el.querySelector(settings.btnPrevSelector);
+      me.btnNext = me.el.querySelector(me.settings.btnNextSelector);
+      me.btnPrev = me.el.querySelector(me.settings.btnPrevSelector);
+      me.animateId = null;
+      me.lastControlEl = me.settings.useController ? me.useIndicator ? me.indicators[me.indicators.length - 1] : me.btnNext : null;
 
       if(!me.sliderWrapper)
-        throw new Error(tag, 'The slider wrapper is absent!', 'selector : ' + settings.wrapperSelector);
+        throw new Error(tag, 'The slider wrapper is absent!', 'selector : ' + me.settings.wrapperSelector);
       if(!me.slideItems|| !me.slideItems.length)
-        throw new Error(tag, 'The slider Items is absent!', 'selector : ' + settings.itemSelector);
-      if(settings.useIndicator && !me.indicator)
-        throw new Error(tag, 'The indicators is absent!', 'selector : ' + settings.indicatorSelector);
-      if(settings.useSRNotation && !me.srNotation)
+        throw new Error(tag, 'The slider Items is absent!', 'selector : ' + me.settings.itemSelector);
+      if(me.settings.useIndicator && !me.indicators.length)
+        throw new Error(tag, 'The indicators is absent!', 'selector : ' + me.settings.indicatorSelector);
+      if(me.settings.useSRNotation && !me.srNotation)
         throw new Error(tag, 'The notation element for SR is absent!');
-      if(!settings.useSRNotation && me.srNotation)
+      if(!me.settings.useSRNotation && me.srNotation)
         me.srNotation.parentElement.removeChild(me.srNotation);
 
-      if(settings.startIdx < 0 || settings.startIdx > me.slideItems.length - 1)
+      if(me.settings.startIdx < 0 || me.settings.startIdx > me.slideItems.length - 1)
         throw new Error(tag, 'The Start Index must be in 0 ~ ' + me.slideItems.length);
       else
-        me.activatedIdx = Number(settings.startIdx);
+        me.activatedIdx = Number(me.settings.startIdx);
 
       me.slideItemWidth = me.slideItems[0].clientWidth;
 
-      me.nextSlide = me.nextSlide.bind(me);
-      me.prevSlide = me.prevSlide.bind(me);
-      if(me.btnNext) me.btnNext.addEventListener('click', me.nextSlide, false);
-      if(me.btnPrev) me.btnPrev.addEventListener('click', me.prevSlide, false);
+      if(me.settings.useController) {
+        me.nextSlide = me.nextSlide.bind(me);
+        me.prevSlide = me.prevSlide.bind(me);
+        if(me.btnNext) me.btnNext.addEventListener('click', me.nextSlide, false);
+        if(me.btnPrev) me.btnPrev.addEventListener('click', me.prevSlide, false);
+      }
 
-      me.animateId = null;
-
+      me.bindKeyEvent();
       me.goSlide(me.activatedIdx);
       return me;
     },
@@ -132,7 +138,7 @@ define(() => {
       if (!this.srNotation) return this;
       let totalItems = this.slideItems.length;
       let el = document.createElement('div');
-      el.textContent = `${idx + 1} / ${totalItems}`;
+      el.textContent = `${idx + 1} / ${totalItems} Slide`;
       if(this.srNotation.firstElementChild)
         this.srNotation.removeChild(this.srNotation.firstElementChild);
       this.srNotation.appendChild(el);
@@ -147,6 +153,40 @@ define(() => {
           item.setAttribute('tabindex', '-1');
         }
       });
+    },
+    bindKeyEvent () {
+      const focusToPannel = ((event) => {
+        event = event || window.event;
+        const el = event.currentTarget;
+        const keycode = event.keyCode || event.which;
+
+        if(keycode === 9 & !event.shiftKey) {
+          event.preventDefault();
+          this.slideItems[this.activatedIdx].focus();
+        }
+      });
+
+      const triggingController = ((event) => {
+        event = event || window.event;
+        const el = event.currentTarget;
+        const keycode = event.keyCode || event.which;
+
+        switch (keycode) {
+          case 37:
+            event.preventDefault();
+            this.prevSlide();
+            break;
+          case 39:
+            event.preventDefault();
+            this.nextSlide();
+            break;
+        }
+      });
+
+      this.controller.addEventListener('keydown', triggingController, false);
+      if (this.lastControlEl)
+        this.lastControlEl.addEventListener('keydown', focusToPannel , false);
+      return this;
     }
   };
 
